e9e1dbe590ac4ed3be4b7b88de3124d931906fb9
rishusingh022 <rishusingh022@gmail.com>
2023-09-03 02:50:21 +0530

    feat: add home screen UI

--- Nuvertos-fe/src/app/app.module.ts
+++ Nuvertos-fe/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule
