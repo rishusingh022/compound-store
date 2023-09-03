754fe8cd3338edd075bafeb830460e030504768b
rishusingh022 <rishusingh022@gmail.com>
2023-09-03 18:37:34 +0530

    feat: add edit modal funtionality

--- Nuvertos-fe/src/app/components/edit-modal/edit-modal.component.ts
+++ Nuvertos-fe/src/app/components/edit-modal/edit-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CompoundService } from '../../services/compound.service';
 
 @Component({
   selector: 'app-edit-modal',
@@ -6,15 +8,26 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
   styleUrls: ['./edit-modal.component.scss']
 })
 export class EditModalComponent {
-  @Input() newCompound: any = {
-    name: '',
-    description: '',
-    imgURL: '',
-  };
+  @Input() compound: any = {};
 
   @Output() close: EventEmitter<void> = new EventEmitter<void>();
 
+  constructor(private route: ActivatedRoute, private compoundService: CompoundService, private router: Router) { }
+
   closeEditModalFunc(){
+    this.close.emit();
+  }
+
+  saveChangesFunc(){
+    const updatedCompound = {
+      id: Number(this.compound.id),
+      compoundName: this.compound.compoundName,
+      compoundDescription: this.compound.compoundDescription,
+      compoundImage: this.compound.compoundImage,
+    };
+    this.compoundService.updateCompound(updatedCompound).subscribe(() => {
+      console.log('Compound updated');
+    }, error => console.log(error.message));
     this.close.emit();
   }
 }
